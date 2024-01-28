5999a018bbd12033f46eaa572965a121d7ba8d31
mgtu7496 <mgtu7496@LinuxROS.myguest.virtualbox.org>
2024-01-28 08:52:16 +0500

    new

--- src/lidar_test/src/lidar_test.py
+++ src/lidar_test/src/lidar_test.py
@@ -5,9 +5,13 @@ from sensor_msgs.msg import LaserScan
 class lidar_test:
     def __init__(self) -> None:
         Subscriber=rospy.Subscriber("/scan",LaserScan,self.callback_scan)
-        
+        self.data = []
+
     def callback_scan(self,msg):
-        print (msg)
+        self.data.append(msg)
+        if len(self.data) == 10:
+            print(self.data)
+            rospy.signal_shutdown("10 data points received")
     
     
     
