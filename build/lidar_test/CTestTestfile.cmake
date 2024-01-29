1f6cf7a462cf0ccea0e119df0fa80bd1f3742429
mgtu7496 <mgtu7496@LinuxROS.myguest.virtualbox.org>
2024-01-29 21:50:18 +0500

    ispravl

---
+++ build/lidar_test/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/mgtu7496/lidar_test/src/lidar_test
+# Build directory: /home/mgtu7496/lidar_test/build/lidar_test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
