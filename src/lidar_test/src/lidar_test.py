f3ac608f633e3cc06ad723f8eda5dcb67b14f644
mgtu7496 <mgtu7496@LinuxROS.myguest.virtualbox.org>
2024-01-29 23:02:15 +0500

    new1

--- src/lidar_test/src/lidar_test.py
+++ src/lidar_test/src/lidar_test.py
@@ -9,7 +9,10 @@ class lidar_test:
 
     def callback_scan(self,msg):
         self.data.append(msg)
-        if len(self.data) == 10:
+        self.data.sort(key=lambda x: x.range, reverse=True) #Сортировка данных по диапазону в порядке убывания
+        if len(self.data) >10:
+            self.data = self.data[:10] #Оставляем 10 верхних точек максимального диапазона
+            self.data.sort(key=lambda x: x.range, reverse=True) #Сортировка данных по диапазону в порядке убывания
             print(self.data)
             rospy.signal_shutdown("10 data points received")
     
