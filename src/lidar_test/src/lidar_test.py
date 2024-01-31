3d3b6e02e106ce0f5ae27105372880dc298266d6
mgtu7496 <mgtu7496@LinuxROS.myguest.virtualbox.org>
2024-01-31 09:50:30 +0500

    var2

--- src/lidar_test/src/lidar_test.py
+++ src/lidar_test/src/lidar_test.py
@@ -4,21 +4,19 @@ from sensor_msgs.msg import LaserScan
 
 class lidar_test:
     def __init__(self) -> None:
-        Subscriber=rospy.Subscriber("/scan",LaserScan,self.callback_scan)
+        self.subscriber = rospy.Subscriber("/scan", LaserScan, self.callback_scan)
         self.data = []
 
-    def callback_scan(self,msg):
+    def callback_scan(self, msg):
         self.data.append(msg)
-        self.data.sort(key=lambda x: x.range, reverse=True) #Сортировка данных по диапазону в порядке убывания
+        self.data.sort(key=lambda x: x.range, reverse=True)
         if len(self.data) >10:
-            self.data = self.data[:10] #Оставляем 10 верхних точек максимального диапазона
-            self.data.sort(key=lambda x: x.range, reverse=True) #Сортировка данных по диапазону в порядке убывания
-            print(self.data)
-            rospy.signal_shutdown("10 data points received")
-    
-    
-    
+            self.data = self.data[:10]
+        print([x.range for x in self.data])
+        rospy.signal_shutdown("10 data points received")
+
+
 if __name__ == '__main__':
     rospy.init_node('control_node')
     mgtu_turtle = lidar_test()
-    rospy.spin()   
+    rospy.spin()
